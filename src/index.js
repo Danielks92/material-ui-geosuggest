c0b9489f1f863c95d2f2e44dee098df7c2aab396
Daniel Körner-Schenk <daniel@lints.at>
2019-02-07 13:59:29 +0100

    removed mu 3.x

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
+import TextField from 'material-ui/TextField';
 import PropTypes from 'prop-types';
 
 class MuiGeoSuggest extends Component {
